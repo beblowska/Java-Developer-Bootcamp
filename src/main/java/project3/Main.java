99c1127aef25aadf51e36f25f8ab0581a05d42a4
Ada <lowskabo@gmail.com>
2023-10-16 22:56:31 +0200

    Fizz Buzz

---
+++ src/main/java/project3/Main.java
@@ -0,0 +1,9 @@
+package project3;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i<=100; i++) {
+            System.out.println(FizzBuzz.play(i));
+        }
+    }
+}
