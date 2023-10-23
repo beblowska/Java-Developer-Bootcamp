0f4debae2ef7d9fa4604c742f639ee906dfe3039
Ada <lowskabo@gmail.com>
2023-10-23 22:45:51 +0200

    Kalkulator walut update

--- src/main/java/project5/CurrencyCalculator.java
+++ src/main/java/project5/CurrencyCalculator.java
@@ -6,10 +6,18 @@ public class CurrencyCalculator {
     private ExchangeRateProvider rateProvider;
 
     public CurrencyCalculator(ExchangeRateProvider rateProvider) {
+
         this.rateProvider = rateProvider;
     }
 
     public double convertToCurrency(double amount, Currency fromCurrency, Currency toCurrency) {
+        double buyingRate = rateProvider.getBuyingExchangeRate(toCurrency);
+        double sellingRate = rateProvider.getSellingExchangeRate(fromCurrency);
+
+        return amount * (buyingRate / sellingRate);
+    }
+
+    public double convert(Currency fromCurrency, Currency toCurrency, double amount) {
         double buyingRate = rateProvider.getBuyingExchangeRate(toCurrency);
         double sellingRate = rateProvider.getSellingExchangeRate(fromCurrency);
 
